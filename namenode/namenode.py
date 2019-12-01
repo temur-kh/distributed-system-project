67e452b473a031f9ef4a594ac611efd90d4abc0f
Rakhimov Abdurasul <rakhimovabdurasul@gmail.com>
2019-12-01 03:17:27 +0300

    client change_directory implemented (#5)
    fixed namenode copy_file and move_file

--- namenode/namenode.py
+++ namenode/namenode.py
@@ -13,34 +13,61 @@ app = Flask(__name__)
 msgs = None
 state = None
 
+def send_message(ip, command='init', arguments=[]):
+    to_send = json.loads('{}')
+    to_send['command'] = command
+    to_send['arguments'] = arguments
+
+    try:
+        response = requests.post(ip, json=to_send, timeout=2)
+    except (requests.ReadTimeout):
+        return 1, ''
+    except (requests.ConnectionError):
+        return 2, ''
+    return 0, response
+
 @app.route('/', methods=['GET', 'POST'])
 def process():
     print('something')
-    message = request.get_json(force=True)
-    print(message)
-    if message is None:
+    omessage = request.get_json(force=True)
+    print(omessage)
+    if omessage is None:
         return msgs.get_rmessage(verdict=1, message='Wrong command format. Use JSON format')
     
     # command = message['command']
     # arguments = message['arguments']
-    print(message)
-    print(message['command'])
+    print(omessage)
+    print(omessage['command'])
     # message_json = json.loads(message)
 
-    code, message = state.tree.perform(message)
+    code, message = state.tree.perform(omessage)
     print(f'code = {code}, message = {message}')
     if code < 2:
         return msgs.get_rmessage(verdict=code, message=message)
-        # return json.loads(f'"verdict": {code}, "message": {message}')
     else:
-        dnode = state.get_available_node()
-        if dnode == None:
-            return msgs.get_rmessage(verdict=1, message="No available datanodes")
+        nTimes = 5
+        while nTimes > 0:
+            dnode = state.get_available_node()
+            if dnode == None:
+                return msgs.get_rmessage(verdict=1, message="No available datanodes")
 
-        else:
-            message = dnode.getAddress()
-            return msgs.get_rmessage(verdict=0, message=message)
-    
+            else:
+                if code == 2:
+                    print('Working in 2')
+                    # msgj = json.loads(omessage)
+                    ver, _ = send_message(dnode.getAddress(), command=omessage['command'], arguments=omessage['arguments'])
+                    if ver == 2:
+                        dnode.setStatus(0)
+                        continue
+                    elif ver == 1:
+                        nTimes -= 1
+                        continue
+                    else:
+                       return msgs.get_rmessage(verdict=0, message=message) 
+                else:
+                    print('Working in 3')
+                    message = dnode.getAddress()
+                    return msgs.get_rmessage(verdict=0, message=message)
 
         
 if __name__ == "__main__":
